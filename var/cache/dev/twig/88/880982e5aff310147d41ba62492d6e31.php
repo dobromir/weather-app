65a1a29562c3ffb678a1b8cb8434746c457a69b7
Dobromir Peev <dobromir@dpanayotov.eu>
2025-01-13 16:16:19 +0200

    Added var dir.

---
+++ var/cache/dev/twig/88/880982e5aff310147d41ba62492d6e31.php
@@ -0,0 +1,146 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* base.html.twig */
+class __TwigTemplate_803ffc5b7ba30050ead241e1b516c092 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        // line 1
+        yield "<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=\"UTF-8\">
+    <style src=\"css/bundle-package.css\"></style>
+    <title>";
+        // line 6
+        yield from $this->unwrap()->yieldBlock('title', $context, $blocks);
+        yield "</title>
+    <!-- Include CSS, Meta tags, etc. -->
+</head>
+<body>
+<div id=\"react-app\">Loading application</div>
+";
+        // line 11
+        yield from $this->unwrap()->yieldBlock('body', $context, $blocks);
+        // line 14
+        yield "
+<script src=\"javascript/bundle-package.js\"></script>
+</body>
+</html>";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        yield from [];
+    }
+
+    // line 6
+    /**
+     * @return iterable<null|scalar|\Stringable>
+     */
+    public function block_title(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        yield "Welcome!";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        yield from [];
+    }
+
+    // line 11
+    /**
+     * @return iterable<null|scalar|\Stringable>
+     */
+    public function block_body(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 12
+        yield "    <!-- Main content for each page goes here -->
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "base.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  102 => 12,  92 => 11,  75 => 6,  64 => 14,  62 => 11,  54 => 6,  47 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=\"UTF-8\">
+    <style src=\"css/bundle-package.css\"></style>
+    <title>{% block title %}Welcome!{% endblock %}</title>
+    <!-- Include CSS, Meta tags, etc. -->
+</head>
+<body>
+<div id=\"react-app\">Loading application</div>
+{% block body %}
+    <!-- Main content for each page goes here -->
+{% endblock %}
+
+<script src=\"javascript/bundle-package.js\"></script>
+</body>
+</html>", "base.html.twig", "/mnt/d/Repos/weather-app/templates/base.html.twig");
+    }
+}
---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/' => [[['_route' => 'home', '_controller' => 'App\\Controller\\WeatherController::index'], null, ['GET' => 0], null, false, false, null]],
+        '/api/weather' => [[['_route' => 'api_post_show', '_controller' => 'App\\Controller\\WeatherController::getWeather'], null, ['GET' => 0, 'HEAD' => 1], null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
